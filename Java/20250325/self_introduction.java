8142da19bcd33380dff35c664a407287204e3333
こんぶ <90184447+Kombuu@users.noreply.github.com>
2025-03-25 10:15:25 +0900

    Create self_introduction.java

---
+++ Java/20250325/self_introduction.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("name: Kirishima");
+
+        System.out.println("language: Ruby, English");
+
+        System.out.println("hitokoto: がんばります!");
+    }
+}
