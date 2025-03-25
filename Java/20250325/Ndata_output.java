75d6b15a544c24831a0b58fb638faebb4200ab46
こんぶ <90184447+Kombuu@users.noreply.github.com>
2025-03-25 11:43:11 +0900

    Create Ndata_output.java

---
+++ Java/20250325/Ndata_output.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+            
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++){
+            String S = sc.next();
+            System.out.println(S);
+        }
+        
+    }
+}
