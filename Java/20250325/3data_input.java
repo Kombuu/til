e1d52618dd7706c1e45a7ef923192fc06f08a797
こんぶ <90184447+Kombuu@users.noreply.github.com>
2025-03-25 11:27:53 +0900

    Create 3data_input.java

---
+++ Java/20250325/3data_input.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String... args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+        String c = sc.next();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
